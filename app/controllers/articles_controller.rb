814224bdfcfd5df07e2b89177b01d438473f6d08
“Shridhar <shridharulli@gmail.com>
2022-12-19 19:01:54 +0530

    Dec/19/final

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
 
   def show
-    byebug
     @article = Article.find(params[:id])
   end
   
@@ -36,6 +35,12 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 end
